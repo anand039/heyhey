d9b27a42ed0533d0d9a447961643742b1a8d8db4
anand039 <52083598+anand039@users.noreply.github.com>
2023-01-22 15:27:58 +0530

    Update User.java
    function added

--- src/main/java/com/heyhey/pojo/User.java
+++ src/main/java/com/heyhey/pojo/User.java
@@ -1,72 +1,90 @@
 package com.heyhey.pojo;
 
+public class User {
+	private String full_name;
+	private String email;
+	private String dob;
+	private String username;
+	private String image_url;
+	private String password;
+	private String provider;
+	public String uuid;
+	public String password_confirmation;
 
-	public class User{
-	    private String full_name;
-	    private String email;
-	    private String dob;
-	    private String username;
-	    private String image_url;
-	    private String password;
-	    private String provider;
-	    public String uuid;
-	    public String password_confirmation;
-	    
-	    public String getPassword_confirmation() {
-			return password_confirmation;
-		}
-		public void setPassword_confirmation(String password_confirmation) {
-			this.password_confirmation = password_confirmation;
-		}
-		public String getFull_name() {
-			return full_name;
-		}
-		public void setFull_name(String full_name) {
-			this.full_name = full_name;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		public String getDob() {
-			return dob;
-		}
-		public void setDob(String dob) {
-			this.dob = dob;
-		}
-		public String getUsername() {
-			return username;
-		}
-		public void setUsername(String username) {
-			this.username = username;
-		}
-		public String getImage_url() {
-			return image_url;
-		}
-		public void setImage_url(String image_url) {
-			this.image_url = image_url;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		public String getProvider() {
-			return provider;
-		}
-		public void setProvider(String provider) {
-			this.provider = provider;
-		}
-		public String getUuid() {
-			return uuid;
-		}
-		public void setUuid(String uuid) {
-			this.uuid = uuid;
-		}
-		
+	public String getPassword_confirmation() {
+		return password_confirmation;
 	}
 
+	public void setPassword_confirmation(String password_confirmation) {
+		this.password_confirmation = password_confirmation;
+	}
 
+	public String getFull_name() {
+		return full_name;
+	}
+
+	public String getName() {
+
+	}
+
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getImage_url() {
+		return image_url;
+	}
+
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+}
